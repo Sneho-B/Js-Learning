599593d82d4dd36731f0b2572cfe1b353655add9
Sneho-B <snehobjs1749@gmail.com>
2025-01-10 04:06:57 +0000

    adding basic files

---
+++ 01_Basic_Variables.js
@@ -0,0 +1,4 @@
+const accountId= 455789
+let accountName ="Snehoritro"
+var accountGmail ="snehobjs1749@gmail.com"
+console.table([accountId,accountGmail,accountName])
---
+++ 02_Basic_dataType.js
@@ -0,0 +1,5 @@
+"use strict";// uses the newer version of Js
+let var1= null
+let var2
+console.log(typeof var1)
+console.log(typeof var2)
